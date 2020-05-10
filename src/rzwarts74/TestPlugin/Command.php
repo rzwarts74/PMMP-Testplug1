c146ee756c68190dd2d4b98b7c37b7a73a504669
rzwarts74 <38145677+rzwarts74@users.noreply.github.com>
2020-05-10 23:49:17 +0200

    Initial file

---
+++ src/rzwarts74/TestPlugin/Command.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace rzwarts74\TestPlugin;
+
+use pocketmine\command\{Command as Cmd, CommandSender};
+use pocketmine\utils\Config;
+use pocketmine\Player;
+use rzwarts74\TestPlugin\Main;
+
+class Command extends Cmd {
+
+	private $main;
+
+	public function __construct(Main $main){
+		$this->plugin = $main;
+		parent::__construct("testplugin", "Just test if it works!", "/testplugin <string: message>");
+		$this->setPermission("testplugin.command");
+	}
+
+	public function execute(CommandSender $player, string $commandLabel, array $args) : void{
+		$this->p = $this->plugin;
+		if(!$player instanceof Player){
+			if($args == ""){
+				if($this->p->getServer()->getOnlinePlayers() == null){
+					$this->p->getServer()->getLogger()->info("[KickAll] - There are not any players to kick!");
+				}else{
+					foreach($this->p->getServer()->getOnlinePlayers() as $players){
+						$players->kick("", false);
+					}
+					$this->p->getServer()->getLogger()->info("[KickAll] - All players kicked!");
+				}
+			}else{
+				if($this->p->getServer()->getOnlinePlayers() == null){
+					$this->p->getServer()->getLogger()->info("[KickAll] - There are not any players to kick!");
+				}else{
+					foreach($this->p->getServer()->getOnlinePlayers() as $players){
+						$players->kick(implode(" ", $args), false);
+					}
+					$this->p->getServer()->getLogger()->info("[KickAll] - All players kicked!");
+				}
+			}
+		}else{
+			if(!$this->testPermission($player)) return;
+			if($args == ""){
+				foreach($this->p->getServer()->getOnlinePlayers() as $players){
+					$players->kick("", true);
+				}
+			}else{
+				foreach($this->p->getServer()->getOnlinePlayers() as $players){
+					$players->kick(implode(" ", $args), true);
+				}
+			}
+			$this->p->getServer()->getLogger()->info("[KickAll] - All players kicked by \"" . $player->getName(). "\"!");
+		}
+	}
+}
--- src/rzwarts74/TestPlugin/Main.php
+++ src/rzwarts74/TestPlugin/Main.php
@@ -1,2 +1,19 @@
-<>
-fff   
+<?php
+
+/*
+Created by AmmyCoding
+Twitter: @AmmyR
+*/
+
+namespace rzwarts74\TestPlugin;
+
+use pocketmine\plugin\PluginBase;
+use rzwarts74\TestPlugin\Command;
+
+class Main extends PluginBase {
+
+	public function onEnable() : void{
+		$this->getServer()->getCommandMap()->register("testplugin", new Command($this));
+	}
+
+}
